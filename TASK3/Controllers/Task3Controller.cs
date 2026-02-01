4e45a5d701492ccb372fb0199b28b41a954fbb07
hojiakbaruralov293 <hojiakbaruralov293@gmail.com>
2026-02-01 13:14:46 +0500

    fix format

--- TASK3/Controllers/Task3Controller.cs
+++ TASK3/Controllers/Task3Controller.cs
@@ -6,17 +6,36 @@ using System.Numerics;
 public class Task3Controller : ControllerBase
 {
     [HttpGet]
-    public IActionResult Get([FromQuery] string x, [FromQuery] string y)
+    public IActionResult Get([FromQuery] string? x, [FromQuery] string? y)
     {
-        if (!BigInteger.TryParse(x, out var a) ||
-            !BigInteger.TryParse(y, out var b) ||
-            a <= 0 || b <= 0)
-        {
+        if (!IsNaturalDigitsOnly(x) || !IsNaturalDigitsOnly(y))
             return Content("NaN", "text/plain");
-        }
+
+        // endi aniq raqamlar ekanini bilamiz
+        var a = BigInteger.Parse(x!);
+        var b = BigInteger.Parse(y!);
 
         var result = Lcm(a, b);
         return Content(result.ToString(), "text/plain");
+    }
+
+    private static bool IsNaturalDigitsOnly(string? s)
+    {
+        if (string.IsNullOrEmpty(s)) return false;
+
+        // faqat raqamlar bo'lsin
+        for (int i = 0; i < s.Length; i++)
+        {
+            char c = s[i];
+            if (c < '0' || c > '9') return false;
+        }
+
+        // natural: 0 bo'lmasin
+        // "0", "00", "000" ham NaN bo'ladi
+        for (int i = 0; i < s.Length; i++)
+            if (s[i] != '0') return true;
+
+        return false;
     }
 
     private static BigInteger Gcd(BigInteger a, BigInteger b)
@@ -34,7 +53,5 @@ public class Task3Controller : ControllerBase
     }
 
     private static BigInteger Lcm(BigInteger a, BigInteger b)
-    {
-        return (a / Gcd(a, b)) * b;
-    }
+        => (a / Gcd(a, b)) * b;
 }
