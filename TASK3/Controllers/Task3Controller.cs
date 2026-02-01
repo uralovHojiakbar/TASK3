511dc95c216dbdaaf23d2e44365f5e088c032848
hojiakbaruralov293 <hojiakbaruralov293@gmail.com>
2026-02-01 12:43:38 +0500

    Update route to email-based path

--- TASK3/Controllers/Task3Controller.cs
+++ TASK3/Controllers/Task3Controller.cs
@@ -1,35 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
 
 [ApiController]
-[Route("uralovhojiakbar698@gmail.com")]
+[Route("uralovhojiakbar698_gmail_com")]
 public class Task3Controller : ControllerBase
 {
     [HttpGet]
-    public string Get([FromQuery] string x, [FromQuery] string y)
+    public IActionResult Get([FromQuery] string x, [FromQuery] string y)
     {
-        if (!int.TryParse(x, out int a) ||
-            !int.TryParse(y, out int b) ||
+        if (!BigInteger.TryParse(x, out var a) ||
+            !BigInteger.TryParse(y, out var b) ||
             a <= 0 || b <= 0)
         {
-            return "NaN";
+            return Content("NaN", "text/plain");
         }
 
-        return Lcm(a, b).ToString();
+        var result = Lcm(a, b);
+        return Content(result.ToString(), "text/plain");
     }
 
-    private int Gcd(int a, int b)
+    private static BigInteger Gcd(BigInteger a, BigInteger b)
     {
+        a = BigInteger.Abs(a);
+        b = BigInteger.Abs(b);
+
         while (b != 0)
         {
-            int t = b;
+            var t = b;
             b = a % b;
             a = t;
         }
         return a;
     }
 
-    private int Lcm(int a, int b)
+    private static BigInteger Lcm(BigInteger a, BigInteger b)
     {
-        return a / Gcd(a, b) * b;
+        return (a / Gcd(a, b)) * b;
     }
 }
